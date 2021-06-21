00b2cf471f3ff0a01d6edd2550669e3f445ba3b0
DESKTOP-2B0SMLO\RB <wisestonster@gmail.com>
2021-06-21 18:31:49 +0900

    Error 처리

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -9,5 +10,12 @@ def index(request):
     return render(request, 'foods/index.html', context=context)
 
 def food_detail(request, food):
-    context = {"name": food}
+    context = dict()
+    if food == "chicken":
+        context["name"] = "코딩에 빠진 닭"
+        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격 !"
+        context["price"] = 10000
+        context["img_path"] = "foods/images/chicken.jpg"
+    else:
+        raise Http404("이런 음식은 없다구요 !")
     return render(request, 'foods/detail.html', context=context)
