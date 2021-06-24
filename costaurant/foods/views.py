ebb4121ed3c77f49f7dcec7eae8ff1dcc9f850c1
DESKTOP-F72FJ8C\wises <wisestonster@gmail.com>
2021-06-24 18:31:35 +0900

    Model. 모두 적용

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.http import Http404
+from foods.models import Menu
 
 # Create your views here.
 def index(request):
+    context = dict()
     today = datetime.today().date()
-    context = {"date": today}
+    menus = Menu.objects.all()
+    context["date"] = today
+    context["menus"] = menus
     return render(request, 'foods/index.html', context=context)
 
-def food_detail(request, food):
+def food_detail(request, pk):
     context = dict()
-    if food == "chicken":
-        context["name"] = "코딩에 빠진 닭"
-        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격 !"
-        context["price"] = 10000
-        context["img_path"] = "foods/images/chicken.jpg"
-    else:
-        raise Http404("이런 음식은 없다구요 !")
+    menu = Menu.objects.get(id=pk)
+    context["menu"] = menu
+
     return render(request, 'foods/detail.html', context=context)
